a6577b673073cb52a33b18f0008d93e3f6e58a57
AlexEarley <alex.earley@concurrents.com>
2022-03-31 15:40:30 -0400

    chagned my-profile BG color

--- src/app/remote-entry/entry.component.ts
+++ src/app/remote-entry/entry.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mfe-poc-my-profile-entry',
   template: `<div class="remote-entry">
-    <h2>my-profile's Remote Entry Component</h2>
+    <h2>my-profile's Remote Entry Component (LOCALHOST)</h2>
   </div>`,
   styles: [
     `
       .remote-entry {
-        background-color: #143055;
+        background-color: #551430;
         color: white;
         padding: 5px;
       }
